1ebad4b7faa91aeff8503cb3c0cd6929d43c7125
jaffer <75342062+J-992@users.noreply.github.com>
2021-11-19 09:37:17 -0500

    Update Sweep_Test.ino

--- Sweep_Test.ino
+++ Sweep_Test.ino
@@ -14,31 +14,34 @@ Servo servo10;
 int pos = 0;    // variable to store the servo position
 
 void setup() {
+  // attaches the servo on pin ## to the servo object
   servo3.attach(3);
-  servo5.attach(5);  // attaches the servo on pin 9 to the servo object
+  servo5.attach(5);  
   servo6.attach(6);
   servo9.attach(9);
   servo10.attach(10);
 }
 
 void loop() {
-  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
+  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees in steps of 1 degree
+    // tell servo to go to position in variable 'pos'
     servo3.write(pos);
-    servo5.write(pos);             // tell servo to go to position in variable 'pos'
+    servo5.write(pos);             
     servo6.write(pos);
     servo9.write(pos);
     servo10.write(pos);
-    delay(15);                       // waits 15 ms for the servo to reach the position
+    // waits 15 ms for the servo to reach the position
+    delay(15);                       
   }
   for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
     servo3.write(pos);
-    servo5.write(pos); // tell servo to go to position in variable 'pos'
+    servo5.write(pos);
     servo6.write(pos);
     servo9.write(pos);
     servo10.write(pos);
     delay(15);                       // waits 15 ms for the servo to reach the position
   }
 
+  //exits so that the code doesnt repeat constantly
   exit(0);
 }
